931c7d946494f30aa32e77bd7b7223f2f2d11e33
Tusar <tusarahmed141@gmail.com>
2022-09-18 00:12:53 +0600

    Solve math operation problem

---
+++ assets/c/math-operation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int A, B;
+  printf("Enter first number: ");
+  scanf("%d", &A);
+  printf("Enter second number: ");
+  scanf("%d", &B);
+
+  printf("Summation = %d\n", A + B);
+  printf("Subtraction = %d\n", A - B);
+  printf("Multiplication = %d\n", A * B);
+  printf("Divide = %d\n", A / B);
+
+  return 0;
+}
