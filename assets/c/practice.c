2a3b9527998abca165475107284d6f6a7144f2c5
Tusar <tusarahmed141@gmail.com>
2022-09-19 23:22:19 +0600

    Explore formal specifier

---
+++ assets/c/practice.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define PI 3.14159
+
+int main() {
+    // Write C code here
+    float num1 = 51, num2 = 3;
+    
+    printf("%.2f\n", PI);
+    printf("Devide = %.2f", num1 / num2);
+
+    return 0;
+}
