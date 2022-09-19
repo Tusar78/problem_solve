d87db1e807fabceaef513c0b282fce3bd2c9fa57
Tusar <tusarahmed141@gmail.com>
2022-09-19 23:24:15 +0600

    Declare constant

--- assets/c/practice.c
+++ assets/c/practice.c
@@ -5,7 +5,10 @@ int main() {
     // Write C code here
     float num1 = 51, num2 = 3;
     
+    const float e = 2.71828;
+    
     printf("%.2f\n", PI);
+    printf("%.2f\n", e);
     printf("Divide = %.2f", num1 / num2);
 
     return 0;
