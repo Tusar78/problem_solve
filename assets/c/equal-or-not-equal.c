8ba830bb45db303583dea26d79642ed5a2dc0c96
Tusar <tusarahmed141@gmail.com>
2022-09-20 23:58:51 +0600

    User input done

--- assets/c/equal-or-not-equal.c
+++ assets/c/equal-or-not-equal.c
@@ -5,5 +5,10 @@
 int main () {
   // Declared variable 
   int num1, num2;
-  
+
+  // User input 
+  printf("Enter your first positive number: ");
+  scanf("%d", num1);
+  printf("Enter your first positive number: ");
+  scanf("%d", num2);
 }
