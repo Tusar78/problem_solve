61afb80597c85779819914540e2c5475b0dfa8c6
Tusar <tusarahmed141@gmail.com>
2022-09-21 00:01:37 +0600

    Equal or not equal

--- assets/c/equal-or-not-equal.c
+++ assets/c/equal-or-not-equal.c
@@ -9,6 +9,15 @@ int main () {
   // User input 
   printf("Enter your first positive number: ");
   scanf("%d", num1);
-  printf("Enter your first positive number: ");
+  printf("Enter your second positive number: ");
   scanf("%d", num2);
+
+  // Condition
+  if (num1 != num2) {
+    printf("Two numbers are not equal");
+  } else {
+    printf("Two numbers are equal");
+  }
+
+  return 0;
 }
