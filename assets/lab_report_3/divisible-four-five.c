0b2ed705df8f364c3d45c430798cce8dc65790f8
Tusar <tusarahmed141@gmail.com>
2022-09-21 22:07:54 +0600

    Divisible by 4 and five between 1 to 100 problem solve

---
+++ assets/lab_report_3/divisible-four-five.c
@@ -0,0 +1,22 @@
+// Write a c program to find out which number is divisible by 4 and 5 between 1 to  100.
+
+#include <stdio.h>
+
+int main () {
+  // Declared variable
+  int i;
+  
+  // Fixed value of 100;
+  const int number = 100;
+  
+  printf("Divisible by 4 and 5 between 1 to  100.\n");
+  
+  // Display 
+  for(i = 1; i <= number; i++) {
+    if (i % 4 == 0 && i % 5 == 0) {
+        printf("%d divisible by 4 and 5 \n", i);
+    }
+  }
+  
+  return 0;
+}
