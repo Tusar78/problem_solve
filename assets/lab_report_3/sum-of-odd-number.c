885737b1dec80b8928d1fba04a4c968c570bbffe
Tusar <tusarahmed141@gmail.com>
2022-09-21 22:01:28 +0600

    Sum of odd number

--- assets/lab_report_3/sum-of-odd-number.c
+++ assets/lab_report_3/sum-of-odd-number.c
@@ -1,4 +1,4 @@
-// Write a c program to find out the sum of even series 2 + 4 + 6 + . . . . +n;
+// Write a c program to find out the sum of odd series 1 + 3 + 5 + . . . . +n;
 
 #include <stdio.h>
 
@@ -7,7 +7,7 @@ int main () {
   int i, num, sum = 0;
 
   // User input
-  printf("Enter an integer number: ");
+  printf("Enter an positve integer number: ");
   scanf("%d", &num);
   
   // User entered number
@@ -15,7 +15,7 @@ int main () {
 
   // display the series 
   for(i = 1; i <= num; i++) {
-    if (i % 2 == 0) {
+    if (i % 2 != 0) {
         sum += i;
         printf("%d + ", i);
     }
