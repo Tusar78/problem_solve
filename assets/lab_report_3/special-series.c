26b3ac41344874714802cc4cf5fff12470f5d7d8
Tusar <tusarahmed141@gmail.com>
2022-09-22 23:26:55 +0600

    Small update on special series

--- assets/lab_report_3/special-series.c
+++ assets/lab_report_3/special-series.c
@@ -3,21 +3,24 @@
 #include <stdio.h>
 
 void main() {   
-    // Declared variable
-    long int i, range, userTerms, terms = 0;
+  // Declared variable
+  long int i, range, userTerms, terms = 0;
 	int sum = 0;
-	
+
 	printf("Enter the number of range: ");
 	scanf("%ld", &range);
-	
+
 	printf("Enter the number of terms: ");
 	scanf("%ld",&userTerms);
-	
+
 	for (i = 1; i <= range; i++) { 
 	  terms = terms * 10 + userTerms;
 	  printf("%ld ", terms);
 	  sum += terms;
 	}
-	
+
 	printf("\nThe sum of the series = %d \n",sum);
+
+  return 0;
 } 
+
