a59f67cdb8fedd0a53647b08d97bd4fa6e27dc2b
Tusar <tusarahmed141@gmail.com>
2022-09-22 21:48:35 +0600

    Divisible four and five and also summation these numbers

--- assets/lab_report_3/divisible-four-five-sum.c
+++ assets/lab_report_3/divisible-four-five-sum.c
@@ -1,10 +1,10 @@
-// Write a c program to find out which number is divisible by 4 and 5 between 10 to 100.
+// Write a c program to find out which number is divisible by 4 and 5 between 10 to 100 and summation these numbers.
 
 #include <stdio.h>
 
 int main () {
   // Declared variable
-  int i;
+  int i, sum = 0;
   
   // Fixed value of 100;
   const int number = 100;
@@ -14,9 +14,12 @@ int main () {
   // Display 
   for(i = 10; i <= number; i++) {
     if (i % 4 == 0 && i % 5 == 0) {
+        sum += i;
         printf("%d divisible by 4 and 5 \n", i);
     }
   }
+  
+  printf("Summation these numbers: %d", sum);
   
   return 0;
 }
