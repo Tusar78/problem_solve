7c6c69e96d0a3ce81c4e13fe12c99b67aae70398
Tusar <tusarahmed141@gmail.com>
2022-09-30 11:04:40 +0600

    Positive or negative

--- assets/c/class-test-prepare.c
+++ assets/c/class-test-prepare.c
@@ -12,25 +12,41 @@
 //   return 0;
 // }
 
+// #include <stdio.h>
+
+// int main() {
+//   int a, b, add, sum, mul;
+//   float div;
+//   printf("Enter first number: ");
+//   scanf("%d", &a);
+//   printf("Enter second number: ");
+//   scanf("%d", &b);
+
+//   add = a + b;
+//   sum = a - b;
+//   mul = a * b;
+//   div = (float) a / b;
+
+//   printf("\nAddition is : %d\n", add);
+//   printf("\nSubtraction is : %d\n", sum);
+//   printf("\nMultiplication is : %d\n", mul);
+//   printf("\nDivision is : %.2f\n", div);
+
+//   return 0;
+// }
+
 #include <stdio.h>
 
 int main() {
-  int a, b, add, sum, mul;
-  float div;
-  printf("Enter first number: ");
-  scanf("%d", &a);
-  printf("Enter second number: ");
-  scanf("%d", &b);
-
-  add = a + b;
-  sum = a - b;
-  mul = a * b;
-  div = (float) a / b;
-
-  printf("\nAddition is : %d\n", add);
-  printf("\nSubtraction is : %d\n", sum);
-  printf("\nMultiplication is : %d\n", mul);
-  printf("\nDivision is : %.2f\n", div);
-
-  return 0;
+    int a;
+    printf("Enter your number: ");
+    scanf("%d", &a);
+    
+    if (a > 0) {
+        printf("\nThis is positive number\n");
+    } else {
+        printf("\nThis is negative number\n");
+    }
+    
+    return 0;
 }
