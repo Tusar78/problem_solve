3acda2e04853abe73dbf565828d4f88fbf909a29
Tusar <tusarahmed141@gmail.com>
2022-09-30 12:13:57 +0600

    Prime number 1 to 100

--- assets/c/class-test-prepare.c
+++ assets/c/class-test-prepare.c
@@ -35,20 +35,56 @@
 //   return 0;
 // }
 
+// #include <stdio.h>
+
+// int main() {
+//     int a;
+//     printf("Enter your number: ");
+//     scanf("%d", &a);
+    
+//     if (a > 0) {
+//         printf("\nThis is positive number\n");
+//     } else if (a == 0) {
+//         printf("\nZero is neither positive or negative\n");
+//     }else {
+//         printf("\nThis is negative number\n");
+//     }
+    
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main() {
+//   int i;
+//   for(i = 1; i <= 10; i++) {
+//     printf("%d ", i);
+//   }
+
+//   return 0;
+// }
+
+// Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
-    int a;
-    printf("Enter your number: ");
-    scanf("%d", &a);
-    
-    if (a > 0) {
-        printf("\nThis is positive number\n");
-    } else if (a == 0) {
-        printf("\nZero is neither positive or negative\n");
-    }else {
-        printf("\nThis is negative number\n");
+    int num, i, count, renge, count2 = 0;
+    scanf("%d", &renge);
+    for (num = 2; num <= renge; num++) {
+        count = 0;
+        for (i = 2; i <= num/2; i++) {
+            if (num % i == 0) {
+                count++;
+                break;
+            }
+        }
+        
+        if (count == 0 && count != 1) {
+            printf("%d ", num);
+            count2++;
+        }
     }
-    
+     
+    printf("\n%d ", count2);
     return 0;
 }
