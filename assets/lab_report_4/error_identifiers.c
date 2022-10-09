3e7b4a7e363ff577248ef07c357e3545936b6080
Tusar <tusarahmed141@gmail.com>
2022-10-09 15:46:21 +0600

    Predict the output of the following code

--- assets/lab_report_4/error_identifiers.c
+++ assets/lab_report_4/error_identifiers.c
@@ -1,11 +1,31 @@
 // Identify the errors and re-write the following code.
 
 #include <stdio.h>
-    int main() {
-    int x, y = 10;
-    scanf("%d", &x);
-    x = x + y;
-    printf("%d\n", x);
+  int main() {
+  int x, y = 10;
+  scanf("%d", &x);
+  x = x + y;
+  printf("%d\n", x);
     
     return 0;
 }
+
+// Predict the output of the following program.
+#include <stdio.h>;
+int main() { 
+  int a=6;
+  printf("a = %d\n",a++);
+  printf("a = %d\n",++a);
+  printf("a = %d\n",a--);
+  printf("a = %d\n",--a);
+
+  return 0;
+}
+
+// Output
+/**
+a = 6
+a = 8
+a = 8
+a = 6
+*/
