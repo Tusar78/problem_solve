e44c7c301b7418325f5dfc6996e1f35fb55809e3
Tusar <tusarahmed141@gmail.com>
2022-10-09 15:14:57 +0600

    Identify the errors and re-write the following code.

---
+++ assets/lab_report_4/error_identifiers.c
@@ -0,0 +1,11 @@
+// Identify the errors and re-write the following code.
+
+#include <stdio.h>
+    int main() {
+    int x, y = 10;
+    scanf("%d", &x);
+    x = x + y;
+    printf("%d\n", x);
+    
+    return 0;
+}
