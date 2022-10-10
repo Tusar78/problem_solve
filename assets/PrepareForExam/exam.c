60b764366efb94c9dd6af853cae2237d948ecbc6
Tusar <tusarahmed141@gmail.com>
2022-10-10 17:14:35 +0600

    Peramide

--- assets/PrepareForExam/exam.c
+++ assets/PrepareForExam/exam.c
@@ -39,3 +39,23 @@ int main() {
 
   return 0;
 }
+
+// Write a C program for printing the following pattern.
+#include <stdio.h>
+
+int main() {
+  int i, j, n;
+  printf("Enter your number: ");
+  scanf("%d", &n);
+
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n - i; j++) {
+      printf(" ");
+    }
+    for (j = 1; j <= i; j++) {
+      printf("* ");
+    }
+    
+    printf("\n");
+  }
+}
