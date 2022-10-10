f3b19435dd0b9b2567ffeb851bf0b26be77646f2
Tusar <tusarahmed141@gmail.com>
2022-10-10 15:54:37 +0600

    Identify the errors and re-write the following code

---
+++ assets/PrepareForExam/exam.c
@@ -0,0 +1,12 @@
+// Indentify the errors and re-write the following code.
+
+#include <stdio.h>
+
+int main() {
+  int x, y = 10;
+  scanf("%d", &x);
+  x = x + y;
+  printf("%d\n", x);
+
+  return 0;
+}
