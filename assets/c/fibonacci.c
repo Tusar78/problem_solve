155d3ee80d3abff9daa924150b89e126bd74ff87
Tusar <tusarahmed141@gmail.com>
2022-11-11 12:39:06 +0600

    Fibonacci in c

---
+++ assets/c/fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+  int fibo[100], n, i;
+  
+  printf("How many number you see fibonacci = ");
+  scanf("%d", &n);
+  
+  fibo[0] = 0;
+  fibo[1] = 1;
+  
+  for(i = 2; i < n; i++) {
+      fibo[i] = fibo[i - 1] + fibo[i - 2];
+  }
+  
+  for(i = 0; i < n; i++) {
+      printf("%d ", fibo[i]);
+  }
+  return 0;    
+}
