6bcf218173c21f6cd740fd79d09465e1da098ff5
Tusar <tusarahmed141@gmail.com>
2022-12-07 16:12:44 +0600

    Base Power recurtion

---
+++ assets/AssignmentTwo/C/basePowerRecursion.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int equation = 1, i = 1;
+void value(int base, int power){
+ equation *= base;
+  
+ if(i == power){
+   printf("%d^%d = %d", base, power, equation);
+   return;
+ }  
+ i++;
+  
+ value(base, power);
+}
+
+int main(){
+ int x, n;
+  
+ printf("Enter the value of Base : ");
+ scanf("%d", &x);
+ printf("Enter the value of Power : ");
+ scanf("%d", &n);
+  
+ value(x,n);
+}
+
