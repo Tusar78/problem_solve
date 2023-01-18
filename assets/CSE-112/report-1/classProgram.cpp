7659b0c1d814e4a9ca3c50e17a5d643bb1734e84
Tusar <tusarahmed141@gmail.com>
2023-01-18 15:51:22 +0600

    Class program done

---
+++ assets/CSE-112/report-1/classProgram.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class shape {
+  double PI = 3.14159;
+  double radius, height, width;
+public: 
+  void setFunc(double radius1, double height1, double width1) {
+    radius = radius1;
+    height = height1;
+    width = width1;
+  }
+  
+  double circleMethod() {
+    double area = PI * radius * radius;
+    return area;
+  }
+  
+  double triangleMethod() {
+    double area = 0.5 * width * height;
+    return area;
+  }
+  
+  double rectangleMethod() {
+    double area = height * width;
+    return area;
+  }
+  
+  void show() {
+    cout << "Area of Circle: " << circleMethod() << "\n";
+    cout << "Area of Triangle: " << triangleMethod() << "\n";
+    cout << "Area of Rectangle: " << rectangleMethod() << "\n";
+  }
+};
+
+int main() {
+  double radius, height, width;
+  
+  cout << "Enter radius : ";
+  cin >> radius;
+  cout << "Enter height: ";
+  cin >> height;
+  cout << "Enter width: ";
+  cin >> width;
+
+  shape variusMethod;
+  variusMethod.setFunc(radius, height, width);
+  variusMethod.show();
+}
